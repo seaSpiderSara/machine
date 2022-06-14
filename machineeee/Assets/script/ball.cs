5227218c998896dcf2af36883e8d56e58b814dc0
seaSpiderSara <am9106@nyu.edu>
2022-06-14 09:57:17 -0400

    Update ball.cs

--- machineeee/Assets/script/ball.cs
+++ machineeee/Assets/script/ball.cs
@@ -6,11 +6,7 @@ public class ball : MonoBehaviour
 {
      public KeyCode ButtonSpace = KeyCode.Space;
      public Rigidbody2D ball_1;
-  //   public gameObject circle_1st;
 
-    //public gameObject FirstBall;
-
-    // Update is called once per frame
      void Update()
     {
         if (Input.GetKeyDown(ButtonSpace))
